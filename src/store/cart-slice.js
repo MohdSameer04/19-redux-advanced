3e44ac7dc9d42b993c51324ddbb0260330187a65
Mohd Sameer <sa5802257@gmail.com>
2024-04-18 11:12:14 +0530

    initial commit

---
+++ src/store/cart-slice.js
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+        changed: false,
+    },
+    reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
+
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            // check if new item is already into array or not 
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+            state.totalQuantity++;
+            state.changed = true;
+
+            // if the item was new the push the new data iinto an array
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
+            if (existingItem.quantity === 1) {
+                // if the id not match which you remove the item then with the help of that we filter out that item 
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        },
+    },
+});
+
+
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
